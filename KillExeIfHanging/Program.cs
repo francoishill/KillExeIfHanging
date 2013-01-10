fe5ad57858d7d65f57fb2d43c9ef50e0276eec72
francois <francois@96be7d1c-b0ca-3742-84c9-dfa957e951b5>
2013-01-10 21:47:42 +0000

    git-svn-id: svn://localhost/killexeifhanging/trunk@3 96be7d1c-b0ca-3742-84c9-dfa957e951b5

--- KillExeIfHanging/Program.cs
+++ KillExeIfHanging/Program.cs
@@ -13,23 +13,27 @@ namespace KillExeIfHanging
 		private static void ShowError(string err)
 		{
 			MessageBox.Show(err, "Error in KillExeIfHanging", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			err = null;
 		}
 
 		private static readonly string cUsageExample = 
 			"Usage as follows:"
 			+ Environment.NewLine
-			+ "KillExeIfHanging.exe -minuteshanging -procname1 -procname2 ... -procnameN"
+			+ "KillExeIfHanging.exe -MinutesWhenTriggeredAsHanging -procname1 -procname2 ... -procnameN"
 			+ Environment.NewLine
 			+ Environment.NewLine
 			+ "Example would be:"
 			+ Environment.NewLine
 			+ "KillExeIfHanging.exe -60 -git -ssh";
 
-		private static int tmpMinutesHanging;
+		private static int minutesWhenTriggeredAsHanging;
 		private static List<string> procnames;
 		private static readonly char[] cAllowedArgumentsStartChars = new char[] { '-', '/' };
 		static int Main(string[] args)
 		{
+			//SharedClasses.AutoUpdating.CheckForUpdates_ExceptionHandler();
+			//We do not check for updates because we do not install it on the GLS build machine, otherwise we need to install AutoUpdater and so forth as well
+
 			if (args.Length == 0 || args.Length < 2)//We need the MinutesHanging and at least ONE processname
 			{
 				ShowError("Please specify at least 2 command-line arguments (" + args.Length + " given)."
@@ -40,9 +44,9 @@ namespace KillExeIfHanging
 			}
 
 			string argumentforMinutesHanging = args[0].TrimStart(cAllowedArgumentsStartChars);
-			if (!int.TryParse(argumentforMinutesHanging, out tmpMinutesHanging))
+			if (!int.TryParse(argumentforMinutesHanging, out minutesWhenTriggeredAsHanging))
 			{
-				ShowError("Unable to parse first argument to int for MinutesHanging, argument = '" + argumentforMinutesHanging + "'. "
+				ShowError("Unable to parse first argument to int for MinutesWhenTriggeredAsHanging, argument text = '" + argumentforMinutesHanging + "'. "
 					+ cUsageExample);
 				return 1;
 			}
@@ -52,7 +56,7 @@ namespace KillExeIfHanging
 				procnames.Add(args[i].TrimStart(cAllowedArgumentsStartChars));
 
 			Timer timerCheckForProcessesHanging = new Timer();
-			timerCheckForProcessesHanging.Interval = 5000;//1000 * 60;//Minutely
+			timerCheckForProcessesHanging.Interval = 1000 * 60;//Minutely
 			timerCheckForProcessesHanging.Tick += new EventHandler(checkForProcessesHanging_Tick);
 			timerCheckForProcessesHanging.Start();
 
@@ -71,24 +75,42 @@ namespace KillExeIfHanging
 					string procNameWithExe = pn + (pn.EndsWith(".exe", StringComparison.InvariantCultureIgnoreCase) ? "" : ".exe");
 					string procNameNoExe = pn.EndsWith(".exe", StringComparison.InvariantCultureIgnoreCase) ? pn.Substring(0, pn.Length - ".exe".Length) : pn;
 					var procs = Process.GetProcessesByName(procNameWithExe);
-					if (procs.Length == 0)
-						procs = Process.GetProcessesByName(procNameNoExe);
-
-					foreach (var proc in procs)
+					try
 					{
-						try
+						if (procs.Length == 0)
+							procs = Process.GetProcessesByName(procNameNoExe);
+
+						foreach (var proc in procs)
 						{
-							if (DateTime.Now.Subtract(proc.StartTime).TotalMinutes >= tmpMinutesHanging)
-								proc.Kill();
+							try
+							{
+								double totalRunningMinutes = DateTime.Now.Subtract(proc.StartTime).TotalMinutes;
+								if (totalRunningMinutes >= minutesWhenTriggeredAsHanging)
+								{
+									proc.Kill();
+									SharedClasses.Logging.LogWarningToFile(
+										string.Format("Hanging process killed, process name = '{0}', total running minutes = {1} (hanging is defined >= {2})", pn, totalRunningMinutes, minutesWhenTriggeredAsHanging),
+										SharedClasses.Logging.ReportingFrequencies.Daily,
+										"KillExeIfHanging");
+								}
+							}
+							catch (Exception exc)
+							{
+								ShowError("Exception in killing process '" + pn + "': " + exc.Message
+									+ Environment.NewLine + Environment.NewLine
+									+ "Stacktrace:"
+									+ Environment.NewLine
+									+ exc.StackTrace);
+							}
 						}
-						catch (Exception exc)
-						{
-							ShowError("Exception in killing process '" + pn + "': " + exc.Message
-								+ Environment.NewLine + Environment.NewLine
-								+ "Stacktrace:"
-								+ Environment.NewLine
-								+ exc.StackTrace);
-						}
+					}
+					finally
+					{
+						procNameWithExe = null;
+						procNameNoExe = null;
+						procs = null;
+						GC.Collect();
+						GC.WaitForPendingFinalizers();
 					}
 				}
 				catch (Exception exc)
